4e7ee4d893853b167e56ab247135ce10928f89d2
Angel Stoykov <angelsto@abv.bg>
2023-04-07 01:29:12 +0300

    refactored

--- client/src/components/header/Logo.js
+++ client/src/components/header/Logo.js
@@ -1,8 +1,8 @@
 const Logo = () => {
     return (
         <div id="logo">
-          <h1><a href="#">Beer Board</a></h1>
-          <h2><a href="#">Celebrate your friendship</a></h2>
+            <h1><a href="/">Beer Board</a></h1>
+            <h2><a href="/">Celebrate your friendship</a></h2>
         </div>
     );
 }
