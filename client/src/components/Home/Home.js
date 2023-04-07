2afaceac47a4b42bb1b2b4fec0e344b08a5af508
Angel Stoykov <angelsto@abv.bg>
2023-04-08 01:03:04 +0300

    added home page picture

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,12 +2,8 @@ import styles from './Home.module.css';
 
 const Home = () => {
     return (
-        <div className={styles.homeImage}>
-            <img src='https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=all' />
-            <h3 id="home-caption" className={styles.sectionWrapper}>
-                It is our great pleasure to have you on board! A hearty welcome to you!
-            </h3>
-
+        <div>
+            <img className={styles.center} src='https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=all' />
         </div>
     );
 }
