28f68527096cc68f3fdf5303fc747d146be6dafd
Angel Stoykov <angelsto@abv.bg>
2023-04-08 01:06:45 +0300

    added h3, edited css

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -2,9 +2,16 @@ import styles from './Home.module.css';
 
 const Home = () => {
     return (
-        <div>
-            <img className={styles.center} src='https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=all' />
-        </div>
+        <>
+            <div>
+                <img className={styles.center} src='https://www.eatthis.com/wp-content/uploads/sites/4/2022/02/Beer-flight.jpg?quality=82&strip=all' />
+            </div>
+            <div>
+                <h3 id="home-caption" className={styles.centerh3}>
+                    It is our great pleasure to have you on board! A hearty welcome to you!
+                </h3>
+            </div>
+        </>
     );
 }
 
