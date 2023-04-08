9f67752c7d934b115d4a624899e9e364712ce04c
angel.stoykov <angel.stoykov@intradiem.com>
2023-04-08 22:33:45 +0300

    added some contact info
    Change-Id: I718b477fb5c89e2dc00b95142aca12069bf8a145

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,7 +1,14 @@
+import { useContext } from "react";
+
+import { AuthContext } from "../../contexts/AuthContext";
+
 const Contact = () => {
+    const { userEmail } = useContext(AuthContext);
+
     return (
-        <div>
-            <h3 className="centerh3">Contact page</h3>
+        <div className="centerh3">
+            <h3>Contact info</h3>
+            <p>You can reach me out at: {userEmail}</p>
         </div>
     );
 }
